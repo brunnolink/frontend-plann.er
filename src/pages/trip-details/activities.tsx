c4a8e3753e955c904c6a2774c2d5adba316b084a
brunno <brunnolinker@gmail.com>
2024-07-15 11:26:12 -0300

    feat: activities from api

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,38 +1,74 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { format, parseISO, getDay } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import { CircleCheck } from "lucide-react";
 
 export function Activities() {
+
+    interface Activity {
+        id: string;
+        title: string;
+        occurs_at: string;
+    }
+
+    const { tripId } = useParams();
+    const [ activities, setActivities ] = useState<Activity[]>([]);
+    
+    useEffect(() => {
+        const fetchActivities = async () => {
+            try {
+                const response = await api.get(`/trips/${tripId}/activities`);
+                console.log("Activities Response:", response.data);
+                setActivities(response.data); 
+            } catch (error) {
+                console.error("Erro ao buscar as atividades:", error);
+            }
+        };
+
+        fetchActivities();
+    }, [tripId]);
+
+    const groupActivitiesByWeekday = (activities: Activity[]): { [key: string]: Activity[] } => {
+        const groupedActivities: { [key: string]: Activity[] } = {};
+
+        activities.forEach(activity => {
+            const date = parseISO(activity.occurs_at);
+            const weekday = format(date, 'EEEE', { locale: ptBR });
+
+            if (!groupedActivities[weekday]) {
+                groupedActivities[weekday] = [];
+            }
+            groupedActivities[weekday].push(activity);
+        });
+
+        return groupedActivities;
+    };
+
+    const groupedActivities = groupActivitiesByWeekday(activities);
+
     return ( 
         <div className="space-y-8">
-        <div className="space-y-2.5">
-            <div className="flex gap-2 items-baseline">
-                <span className="text-xl text-zinc-300 font-semibold">dia 17</span>
-                <span className="text-xs text-zinc-500">sabado</span>
-            </div>
-            <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data</p>
-        </div>
-
-        <div className="space-y-2.5">
-            <div className="flex gap-2 items-baseline">
-                <span className="text-xl text-zinc-300 font-semibold">dia 18</span>
-                <span className="text-xs text-zinc-500">Domingo</span>
-            </div>
-
-            <div className="space-y-2.5">
-                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3 ">
-                    <CircleCheck className="size-5 text-lime-300" />
-                    <span className="text-zinc-100">Academia em grupo </span>
-                    <span className="text-zinc-400 text-sm ml-auto">18:00</span>
+            {Object.keys(groupedActivities).map(weekday => (
+                <div key={weekday} className="space-y-2.5">
+                    <div className="flex gap-2 items-baseline">
+                        <span className="text-xl text-zinc-300 font-semibold">Dia {format(parseISO(groupedActivities[weekday][0].occurs_at), 'd')}</span>
+                        <span className="text-xs text-zinc-500">{weekday}</span>
+                    </div>
+                    {groupedActivities[weekday].map(activity => (
+                        <div key={activity.id} className="space-y-2.5">
+                            <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                                <CircleCheck className="size-5 text-lime-300" />
+                                <span className="text-zinc-100">{activity.title}</span>
+                                <span className="text-zinc-400 text-sm ml-auto">
+                                    {format(parseISO(activity.occurs_at), 'HH:mm')}h
+                                </span>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            </div>
-
-            <div className="space-y-2.5">
-                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3 ">
-                    <CircleCheck className="size-5 text-lime-300" />
-                    <span className="text-zinc-100">Academia em grupo </span>
-                    <span className="text-zinc-400 text-sm ml-auto">18:00</span>
-                </div>
-            </div>
+            ))}
         </div>
-    </div>
     )
 }
--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,5 +1,8 @@
 import { Calendar, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { FormEvent } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
 
 interface CreateActivityModalProps {
     closeCreateActivityModal: () => void;
@@ -9,6 +12,26 @@ interface CreateActivityModalProps {
 export function CreateActivityModal({
     closeCreateActivityModal
 }: CreateActivityModalProps) {
+    
+    const { tripId } = useParams();
+   
+
+    async  function createActivity(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget)
+
+        const title = data.get('title')?.toString()
+        const occurs_at = data.get('occurs_at')?.valueOf()
+        
+        await api.post(`/trips/${tripId}/activities`, {
+            title,
+            occurs_at,
+        })
+
+        window.document.location.reload();
+    }
+
     return (
         <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
         <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
@@ -28,7 +51,7 @@ export function CreateActivityModal({
             </div>
 
             <form
-
+                onSubmit={createActivity}
                 className='space-y-3'>
 
                 <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
@@ -36,7 +59,7 @@ export function CreateActivityModal({
                     <Tag
                         className='text-zinc-400 size-5' />
                     <input
-                        name='title'
+                        name="title"
                         placeholder="Qual a atividade?"
                         className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
                 </div>
@@ -46,7 +69,7 @@ export function CreateActivityModal({
                         <Calendar className='text-zinc-400 size-5' />
                         <input
                             type="datetime-local"
-                            name='occurs_at'
+                            name="occurs_at"
                             placeholder="Data e horário da atividade"
                             className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
 
